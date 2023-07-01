4c8d207003a9006678759e2a6ea153022f76cef2
Y N <69714051+nikolaevje@users.noreply.github.com>
2023-07-01 16:50:49 +0300

    Add base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })]
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  base: "/shopping-list"
 });
